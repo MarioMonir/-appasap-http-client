7e55db28eebf26ef7553dab4708bf991498614eb
MarioMonir <mario3monir@gmail.com>
2024-02-15 01:22:59 +0200

    Initl commit

---
+++ http.client.js
@@ -0,0 +1,51 @@
+"use strict";
+/**
+ * Http Client
+ *
+ * @author MarioMonir
+ */
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.httpClient = void 0;
+// -------------------------------------------------
+const qs_1 = __importDefault(require("qs"));
+// -------------------------------------------------
+// GET /api/user?filter=%7B%7D&range=%5B0%2C9%5D&sort=%5B%22id%22%2C%22ASC%22%5D
+// @link: https://dev.to/nerdyman/replacing-query-string-with-native-urlsearchparams-4kdg
+// -------------------------------------------------
+const httpClient = ({ method = "GET", body = null, headers, url, queryParams = {}, }) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    try {
+        const params = qs_1.default.stringify(queryParams);
+        url = `${url}?${params}`;
+        headers = Object.assign({ "content-type": "application/json" }, headers);
+        if (body instanceof Object) {
+            body = JSON.stringify(body);
+        }
+        const res = yield fetch(url, { method, body, headers });
+        const data = yield res.json();
+        const { ok, status } = res;
+        let total = 10;
+        let contentRangeHeader = (_a = res === null || res === void 0 ? void 0 : res.headers) === null || _a === void 0 ? void 0 : _a.get("Content-Range");
+        if (contentRangeHeader) {
+            total = +(contentRangeHeader === null || contentRangeHeader === void 0 ? void 0 : contentRangeHeader.split("/")[1]) || 10;
+        }
+        return { res, ok, status, data, total };
+    }
+    catch (error) {
+        console.error(error);
+        throw error;
+    }
+});
+exports.httpClient = httpClient;
---
+++ http.client.ts
@@ -0,0 +1,79 @@
+/**
+ * Http Client for AppAsap
+ *
+ * @author MarioMonir
+ */
+
+// -------------------------------------------------
+
+import queryString from "qs";
+
+// -------------------------------------------------
+
+export type HttpClientInput = {
+  url: string;
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  body?: BodyInit | null | undefined | object;
+  headers?: {};
+  queryParams?: any;
+};
+
+export type HttpClientOutput = {
+  res: Response;
+  data: any;
+  ok: boolean;
+  status: number;
+  total: number;
+};
+
+export type HttpClient = (input: HttpClientInput) => Promise<HttpClientOutput>;
+
+// -------------------------------------------------
+
+// GET /api/user?filter=%7B%7D&range=%5B0%2C9%5D&sort=%5B%22id%22%2C%22ASC%22%5D
+// @link: https://dev.to/nerdyman/replacing-query-string-with-native-urlsearchparams-4kdg
+
+// -------------------------------------------------
+
+export const httpClient: HttpClient = async ({
+  method = "GET",
+  body = null,
+  headers,
+  url,
+  queryParams = {},
+}) => {
+  try {
+    const params = queryString.stringify(queryParams);
+
+    url = `${url}?${params}`;
+
+    headers = {
+      "content-type": "application/json",
+      ...headers,
+    };
+
+    if (body instanceof Object) {
+      body = JSON.stringify(body);
+    }
+
+    const res = await fetch(url, { method, body, headers });
+
+    const data = await res.json();
+
+    const { ok, status } = res;
+
+    let total: number = 10;
+
+    let contentRangeHeader = res?.headers?.get("Content-Range");
+
+    if (contentRangeHeader) {
+      total = +contentRangeHeader?.split("/")[1] || 10;
+    }
+
+    return { res, ok, status, data, total };
+  } catch (error) {
+    console.error(error);
+
+    throw error;
+  }
+};
